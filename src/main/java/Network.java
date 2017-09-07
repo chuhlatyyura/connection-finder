3cea933d32361ecfe30683d9c3f009204bea5e0c
yuriyc <yuriyc@pointgrab.com>
2017-09-07 15:29:23 +0300

    Added some tests

--- src/main/java/Network.java
+++ src/main/java/Network.java
@@ -4,76 +4,93 @@ import java.util.Map;
 import java.util.Set;
 
 public class Network {
-	private int size;
-	private Map<Integer, Element> linkedElementsMap;
+    private int size;
+    private Map<Integer, Element> elementsMap;
 
-	public Network(int size) throws IllegalAccessException {
-		if (size < 0) {
-			throw new IllegalArgumentException("Number of elements should be a positive number");
-		}
-		this.size = size;
-		linkedElementsMap = new HashMap<>(size);
-		for (int value = 1; value <= size; value++) {
-			linkedElementsMap.put(value, new Element(value));
-		}
-	}
+    private static class Element {
+        private int value;
+        private Set<Element> links;
 
-	public void connect(int first, int second) {
-		validateParameters(first, second);
-		if (first != second) {
-			Element firstElement = linkedElementsMap.get(first);
-			Element secondElement = linkedElementsMap.get(second);
-			firstElement.links.add(secondElement);
-			secondElement.links.add(firstElement);
-		}
-	}
-
-	public boolean query(int first, int second) {
-		validateParameters(first, second);
-		if (first == second) {
-			return true;
-		}
-		Element firstElement = linkedElementsMap.get(first);
-		HashSet<Integer> explored = new HashSet<>();
-		explored.add(first);
-		return search(firstElement, second, explored);
-	}
-
-	private boolean search(Element source, int target, Set<Integer> explored) {
-		boolean result = false;
-		for (Element element : source.links) {
-			if (explored.contains(element.value)) {
-				continue;
-			}
-			if (element.value == target || result) {
-				return true;
-			} else {
-				explored.add(element.value);
-				result = search(element, target, explored);
-			}
-		}
-		return result;
-	}
+        public Element(int value) {
+            this.value = value;
+            links = new HashSet<>();
+        }
+    }
 
 
-	private void validateParameters(int first, int second) {
-		if (first < 0 || second < 0) {
-			throw new IllegalArgumentException("The values should be a positive number");
-		}
-		if (first > size || second > size) {
-			throw new IllegalArgumentException("The values should not be greater than " + size);
-		}
-	}
+    /**
+     * Generates new network with number of elements passed as parameter
+     *
+     * @param size number of elements to be generated
+     * @throws IllegalAccessException if number of elements is less than zero
+     */
+    public Network(int size) throws IllegalAccessException {
+        if (size < 0) {
+            throw new IllegalArgumentException("Number of elements should be a positive number");
+        }
+        this.size = size;
+        elementsMap = new HashMap<>(size);
+        for (int value = 1; value <= size; value++) {
+            elementsMap.put(value, new Element(value));
+        }
+    }
 
+    /**
+     * Connects two elements
+     *
+     * @param first  first element to be connected
+     * @param second second element to be connected
+     */
+    public void connect(int first, int second) {
+        validateParameters(first, second);
+        if (first != second) {
+            Element firstElement = elementsMap.get(first);
+            Element secondElement = elementsMap.get(second);
+            firstElement.links.add(secondElement);
+            secondElement.links.add(firstElement);
+        }
+    }
 
-	private class Element {
-		private int value;
-		private Set<Element> links;
+    /**
+     * Checks whether two elements are connected directly or indirectly through other elements
+     *
+     * @param first  source element
+     * @param second target element
+     * @return returns true, if elements are connected, otherwise - false
+     */
+    public boolean query(int first, int second) {
+        validateParameters(first, second);
+        if (first == second) {
+            return true;
+        }
+        Element firstElement = elementsMap.get(first);
+        HashSet<Integer> explored = new HashSet<>();
+        explored.add(first);
+        return search(firstElement, second, explored);
+    }
 
-		public Element(int value) {
-			this.value = value;
-			links = new HashSet<>();
-		}
-	}
+    private boolean search(Element source, int target, Set<Integer> explored) {
+        boolean result = false;
+        for (Element element : source.links) {
+            if (explored.contains(element.value)) {
+                continue;
+            }
+            if (element.value == target || result) {
+                return true;
+            } else {
+                explored.add(element.value);
+                result = search(element, target, explored);
+            }
+        }
+        return result;
+    }
 
+    private void validateParameters(int first, int second) {
+        if (first < 0 || second < 0) {
+            throw new IllegalArgumentException("The values should be a positive number");
+        }
+        if (first > size || second > size) {
+            throw new IllegalArgumentException("The values should not be greater than " + size);
+        }
+    }
 }
--- src/test/java/NetworkTest.java
+++ src/test/java/NetworkTest.java
@@ -3,174 +3,263 @@ import org.junit.Test;
 
 public class NetworkTest {
 
-	public static final int NETWORK_SIZE = 20;
-	private Network network;
+    public static final int NETWORK_SIZE = 20;
+    private Network network;
 
-	@Before
-	public void setup() throws Exception {
-		network = new Network(NETWORK_SIZE);
-	}
+    @Before
+    public void setup() throws Exception {
+        network = new Network(NETWORK_SIZE);
+    }
 
-	@Test
-	public void testConstructorValidParameters() throws Exception {
-		new Network(10);
-	}
+    @Test
+    public void testConstructorValidParameters() throws Exception {
+        new Network(10);
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testConstructorNegativeParametersShouldThrowAnException() throws Exception {
-		new Network(-1);
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorNegativeParametersShouldThrowAnException() throws Exception {
+        new Network(-1);
+    }
 
-	@Test
-	public void testConnectValidParameters() throws Exception {
-		network.connect(1, 2);
-	}
+    @Test
+    public void testConnectValidParameters() throws Exception {
+        network.connect(1, 2);
+    }
 
-	@Test
-	public void testConnectToSelf() throws Exception {
-		network.connect(1, 1);
-	}
+    @Test
+    public void testConnectToSelf() throws Exception {
+        network.connect(1, 1);
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testConnectFirstValueGreaterThanElementsNumberShouldThrowAnException() throws Exception {
-		network.connect(NETWORK_SIZE + 1, 1);
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void testConnectFirstValueGreaterThanElementsNumberShouldThrowAnException() throws Exception {
+        network.connect(NETWORK_SIZE + 1, 1);
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testConnectSecondValueGreaterThanElementsNumberShouldThrowAnException() throws Exception {
-		network.connect(1, NETWORK_SIZE + 1);
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void testConnectSecondValueGreaterThanElementsNumberShouldThrowAnException() throws Exception {
+        network.connect(1, NETWORK_SIZE + 1);
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testConnectBothValuesGreaterThanElementsNumberShouldThrowAnException() throws Exception {
-		network.connect(NETWORK_SIZE + 1, NETWORK_SIZE + 1);
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void testConnectBothValuesGreaterThanElementsNumberShouldThrowAnException() throws Exception {
+        network.connect(NETWORK_SIZE + 1, NETWORK_SIZE + 1);
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testConnectFirstNegativeValueShouldThrowAnException() throws Exception {
-		network.connect(1, -1);
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void testConnectFirstNegativeValueShouldThrowAnException() throws Exception {
+        network.connect(1, -1);
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testConnectNegativeSecondValueShouldThrowAnException() throws Exception {
-		network.connect(-1, 1);
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void testConnectNegativeSecondValueShouldThrowAnException() throws Exception {
+        network.connect(-1, 1);
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testConnectBothValuesNegativeShouldThrowAnException() throws Exception {
-		network.connect(-1, -1);
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void testConnectBothValuesNegativeShouldThrowAnException() throws Exception {
+        network.connect(-1, -1);
+    }
 
-	@Test
-	public void testConnectDuplicate() throws Exception {
-		network.connect(1, 2);
-		network.connect(1, 2);
-	}
+    @Test
+    public void testConnectDuplicate() throws Exception {
+        network.connect(1, 2);
+        network.connect(1, 2);
+    }
 
-	@Test
-	public void testQuerySelf() throws Exception {
-		assert network.query(1, 1);
-	}
+    @Test
+    public void testQuerySelf() throws Exception {
+        assert network.query(1, 1);
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testQueryFirsValueGreaterThanSizeShouldThrowAndException() throws Exception {
-		network.query(NETWORK_SIZE + 1, 1);
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void testQueryFirsValueGreaterThanSizeShouldThrowAndException() throws Exception {
+        network.query(NETWORK_SIZE + 1, 1);
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testQuerySecondValueGreaterThanSizeShouldThrowAndException() throws Exception {
-		network.query(NETWORK_SIZE + 1, 1);
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void testQuerySecondValueGreaterThanSizeShouldThrowAndException() throws Exception {
+        network.query(NETWORK_SIZE + 1, 1);
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testQueryBothValuesGreaterThanSizeShouldThrowAndException() throws Exception {
-		network.query(NETWORK_SIZE + 1, NETWORK_SIZE + 1);
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void testQueryBothValuesGreaterThanSizeShouldThrowAndException() throws Exception {
+        network.query(NETWORK_SIZE + 1, NETWORK_SIZE + 1);
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testQueryFirstNegativeValueShouldThrowAnException() throws Exception {
-		assert network.query(-1, 1);
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void testQueryFirstNegativeValueShouldThrowAnException() throws Exception {
+        assert network.query(-1, 1);
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testQuerySecondNegativeValueShouldThrowAnException() throws Exception {
-		assert network.query(1, -1);
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void testQuerySecondNegativeValueShouldThrowAnException() throws Exception {
+        assert network.query(1, -1);
+    }
 
-	@Test
-	public void testQueryOneLevel() throws Exception {
-		network.connect(1, 2);
-		assert network.query(1, 2);
-	}
+    @Test
+    public void testQueryOneLevel() throws Exception {
+        network.connect(1, 2);
+        assert network.query(1, 2);
+    }
 
-	@Test
-	public void testQueryOneLevelChangedOrder() throws Exception {
-		network.connect(1, 2);
-		assert network.query(2, 1);
-	}
+    @Test
+    public void testQueryOneLevelChangedOrder() throws Exception {
+        network.connect(1, 2);
+        assert network.query(2, 1);
+    }
 
-	@Test
-	public void testQueryTwoLevels() throws Exception {
-		network.connect(1, 2);
-		network.connect(2, 3);
-		assert network.query(1, 3);
-	}
+    @Test
+    public void testQueryTwoLevels() throws Exception {
+        network.connect(1, 2);
+        network.connect(2, 3);
+        assert network.query(1, 3);
+    }
 
-	@Test
-	public void testQueryTwoLevelsChangedOrder() throws Exception {
-		network.connect(1, 2);
-		network.connect(2, 3);
-		assert network.query(3, 1);
-	}
+    @Test
+    public void testQueryTwoLevelsChangedOrder() throws Exception {
+        network.connect(1, 2);
+        network.connect(2, 3);
+        assert network.query(3, 1);
+    }
 
-	@Test
-	public void testQueryThreeLevels() throws Exception {
-		network.connect(1, 2);
-		network.connect(2, 3);
-		network.connect(3, 4);
-		assert network.query(1, 4);
-	}
+    @Test
+    public void testQueryThreeLevels() throws Exception {
+        network.connect(1, 2);
+        network.connect(2, 3);
+        network.connect(3, 4);
+        assert network.query(1, 4);
+    }
 
-	@Test
-	public void testQueryThreeLevelsChangeOrder() throws Exception {
-		network.connect(1, 2);
-		network.connect(2, 3);
-		network.connect(3, 4);
-		assert network.query(4, 1);
-	}
+    @Test
+    public void testQueryThreeLevelsChangeOrder() throws Exception {
+        network.connect(1, 2);
+        network.connect(2, 3);
+        network.connect(3, 4);
+        assert network.query(4, 1);
+    }
 
-	@Test
-	public void testQueryThreeLevelsTargetInTheMiddle() throws Exception {
-		network.connect(1, 2);
-		network.connect(2, 3);
-		network.connect(3, 4);
-		assert network.query(1, 3);
-	}
+    @Test
+    public void testQueryThreeLevelsTargetInTheMiddle() throws Exception {
+        network.connect(1, 2);
+        network.connect(2, 3);
+        network.connect(3, 4);
+        assert network.query(1, 3);
+    }
 
-	@Test
-	public void testQueryThreeLevelsBinary() throws Exception {
-		network.connect(1, 2);
-		network.connect(1, 3);
-		network.connect(2, 4);
-		network.connect(2, 5);
-		network.connect(3, 6);
-		network.connect(3, 7);
-		network.connect(4, 8);
-		network.connect(4, 9);
-		assert network.query(1, 8);
-	}
+    @Test
+    public void testQueryThreeLevelsBinary() throws Exception {
+        network.connect(1, 2);
+        network.connect(1, 3);
+        network.connect(2, 4);
+        network.connect(2, 5);
+        network.connect(3, 6);
+        network.connect(3, 7);
+        network.connect(4, 8);
+        network.connect(4, 9);
+        assert network.query(1, 8);
+    }
 
-	@Test
-	public void testQueryThreeLevelsBinaryWithCyclicReferences() throws Exception {
-		network.connect(1, 2);
-		network.connect(1, 3);
-		network.connect(2, 4);
-		network.connect(2, 5);
-		network.connect(3, 6);
-		network.connect(3, 7);
-		network.connect(4, 8);
-		network.connect(4, 9);
-		network.connect(8, 2);
-		assert network.query(1, 8);
-	}
+    @Test
+    public void testQueryOnNetworkOneCheckEightToOne() throws Exception {
+        buildNetworkOne();
+        assert !network.query(8, 1);
+    }
+
+    @Test
+    public void testQueryOnNetworkOneCheckTwelveToNine() throws Exception {
+        buildNetworkOne();
+        assert network.query(12, 9);
+    }
+
+    @Test
+    public void testQueryOnNetworkOneCheckEighteenToFour() throws Exception {
+        buildNetworkOne();
+        assert network.query(18, 4);
+    }
+
+    @Test
+    public void testQueryOnNetworkTwoCheckOneToFourteen() throws Exception {
+        buildNetworkTwo();
+        assert network.query(1, 14);
+    }
+
+    @Test
+    public void testQueryOnNetworkTwoCheckThreeToNine() throws Exception {
+        buildNetworkTwo();
+        assert !network.query(3, 9);
+    }
+
+    @Test
+    public void testQueryOnNetworkTwoCheckFourToNine() throws Exception {
+        buildNetworkTwo();
+        assert !network.query(4, 9);
+    }
+
+    /**
+     * <pre>
+     * 1----2----3    4    5
+     * |\__ |        /
+     * |   \|       /
+     * 10   9    8 |  7    6
+     *   \_      | |/
+     *     \     |/
+     * 11   12   13   14   15
+     *       |__(______|
+     *         /
+     * 16   17---18   19   20
+     * </pre>
+     */
+    private void buildNetworkOne() {
+        network.connect(1, 2);
+        network.connect(1, 9);
+        network.connect(1, 10);
+        network.connect(2, 3);
+        network.connect(10, 12);
+        network.connect(12, 14);
+
+        network.connect(18, 17);
+        network.connect(17, 13);
+        network.connect(13, 8);
+        network.connect(13, 7);
+        network.connect(13, 4);
+    }
+
+    /**
+     * <pre>
+     * 1----2    3    4----5
+     * |    |       /      |
+     * |    |      /       |
+     * 6    7----8    9    10
+     * |    |   / \__      /
+     * |    | /      \   /
+     * 11   12   13   14   15
+     * |     \___    /
+     * |         \ /
+     * 16---17---18---19   20
+     * </pre>
+     */
+    private void buildNetworkTwo() {
+        network.connect(1, 2);
+        network.connect(1, 6);
+        network.connect(6, 11);
+        network.connect(11, 16);
+        network.connect(16, 17);
+        network.connect(17, 18);
+
+        network.connect(18, 19);
+        network.connect(18, 12);
+        network.connect(18, 14);
+
+        network.connect(2, 7);
+        network.connect(7, 8);
+        network.connect(7, 12);
+        network.connect(8, 12);
+        network.connect(8, 14);
+        network.connect(8, 4);
+        network.connect(4, 5);
+        network.connect(5, 10);
+        network.connect(10, 14);
+    }
+
 }
